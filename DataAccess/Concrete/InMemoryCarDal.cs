3597c6263302b9db4b8ea59b4d06e77fba30ca01
Emre YİĞİT <emreygt67@icloud.com>
2021-03-26 15:30:49 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{BrandId=1,ColorId=1,DailyPrice=150000,Description="kazasız",Id=1,ModelYear=2013},
+                new Car{BrandId=2,ColorId=3,DailyPrice=200000,Description="memurdan",Id=2,ModelYear=2003},
+                new Car{BrandId=3,ColorId=5,DailyPrice=300000,Description="10000 tramer kaydı mevcut",Id=3,ModelYear=2019},
+
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByld()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            car.DailyPrice = car.BrandId;
+
+        }
+    }
+}
